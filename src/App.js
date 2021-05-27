26626079367ad0b225a3375f332a485850e98d31
tisukRoman <tisukroman@gmail.com>
2021-05-27 18:51:26 +0300

    without buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { concat, interval, Observable, Subject, of, timer } from 'rxjs';
+import { startWith, scan, takeWhile, repeatWhen, filter, takeUntil, map, retryWhen, tap, delayWhen } from 'rxjs/operators';
+import './App.css'
 
-function App() {
+const action = new Subject(); // actions
+const start = action.pipe(filter(a => a === 'start'));
+const stop = action.pipe(filter(a => a === 'stop'));
+
+let sec, min, hour = 1
+
+const secObserv = interval(200).pipe( // counts till one minute
+  map(sec => {
+    if (sec > 59) {
+      throw sec;
+    }
+    return sec + 1;
+  }),
+  retryWhen(errors =>
+    errors.pipe(
+      tap(sec => console.log(`one minute!`)),
+    )
+  )
+);
+
+const minObserv = interval(12000).pipe( // counts till one hour
+  tap(min=>console.log(min)),
+  map(min => {
+    if (min > 59) {
+      throw min;
+    }
+    return min + 1;
+  }),
+  retryWhen(errors =>
+    errors.pipe(
+      tap(min => console.log(`one hour!`)),
+    )
+  )
+);
+
+const hourObserv = interval(720000).pipe( // counts till 24 hours
+  map(hour => {
+    if (hour > 59) {
+      throw hour;
+    }
+    return hour + 1;
+  }),
+  retryWhen(errors =>
+    errors.pipe(
+      tap(min => console.log(`24 hours!`)),
+    )
+  )
+);
+
+
+
+const counterStop = concat(secObserv, of(0)).pipe(
+  repeatWhen(() => start)
+)
+const observable = concat(counterStop.pipe(
+  takeUntil(stop)), of(0)
+)
+
+
+export default React.memo(function App() {
+
+
+
+  const [seconds, incSecond] = React.useState(0);
+  const [minutes, incMinutes] = React.useState(0);
+  const [hours, incHours] = React.useState(0);
+
+  React.useEffect(() => {
+    /* const sub = observable.subscribe(incSecond);
+    return () => sub.unsubscribe(); */
+    secObserv.subscribe(incSecond);
+    minObserv.subscribe(incMinutes);
+    hourObserv.subscribe(incHours);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="clock">
+        <div> {hours} h </div>
+        <div> : </div>
+        <div> {minutes} m </div>
+        <div> : </div>
+        <div> {seconds} s </div>
+      </div>
+
+      <div className="buttons">
+        <button onClick={() => action.next('start')}> START</button>
+        <button onClick={() => action.next('stop')}>STOP</button>
+      </div>
+
     </div>
   );
-}
+})
 
-export default App;
